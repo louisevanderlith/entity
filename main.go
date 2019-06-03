88afe40e8f279a64e0e07dd78e28d22861d949b8
Louise van der Lith <louisevanderlith@gmail.com>
2019-06-03 22:42:48 +0200

    Removed Runmode

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path"
 
 	"github.com/louisevanderlith/entity/core"
 	"github.com/louisevanderlith/entity/routers"
@@ -13,9 +14,9 @@ import (
 )
 
 func main() {
-	mode := os.Getenv("RUNMODE")
 	keyPath := os.Getenv("KEYPATH")
 	pubName := os.Getenv("PUBLICKEY")
+	host := os.Getenv("HOST")
 	pubPath := path.Join(keyPath, pubName)
 
 	core.CreateContext()
@@ -23,7 +24,7 @@ func main() {
 
 	// Register with router
 	name := beego.BConfig.AppName
-	srv := mango.NewService(mode, name, pubPath, enums.API)
+	srv := mango.NewService(name, pubPath, enums.API)
 
 	port := beego.AppConfig.String("httpport")
 	err := srv.Register(port)
@@ -31,7 +32,7 @@ func main() {
 	if err != nil {
 		log.Print("Register: ", err)
 	} else {
-		routers.Setup(srv)
+		routers.Setup(srv, host)
 		beego.Run()
 	}
 }

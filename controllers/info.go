ca8309e202bbfd233002a1f906ab79b7a67f2a39
Louise van der Lith <louisevanderlith@gmail.com>
2020-01-21 22:44:42 +0200

    info removed...

--- controllers/info.go
+++ controllers/info.go
@@ -1,32 +1,31 @@
 package controllers
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/louisevanderlith/droxo"
+	"github.com/louisevanderlith/squareroot/ctx"
 	"net/http"
 
-	"github.com/louisevanderlith/droxolite/context"
 	"github.com/louisevanderlith/entity/core"
 	"github.com/louisevanderlith/husk"
 )
 
-type Info struct {
-}
-
-func (x *Info) Get(c *gin.Context) {
+func Get(c *gin.Context) {
 	results := core.GetEntities(1, 10)
 
-	return http.StatusOK, results
+	c.JSON(http.StatusOK, results)
 }
 
 // @Title GetEntities
 // @Description Gets the entities
 // @Success 200 {[]core.Entity} []core.Entity
 // @router /all/:pagesize [get]
-func (req *Info) Search(c *gin.Context) {
-	page, size := ctx.GetPageData()
+func Search(c *gin.Context) {
+	page, size := droxo.GetPageData(c.Param("pagesize"))
 
 	results := core.GetEntities(page, size)
 
-	return http.StatusOK, results
+	c.JSON(http.StatusOK, results)
 }
 
 // @Title GetEntity
@@ -34,20 +33,22 @@ func (req *Info) Search(c *gin.Context) {
 // @Param	key			path	husk.Key 	true		"Key of the entity you require"
 // @Success 200 {core.Entity} core.Entity
 // @router /:key [get]
-func (req *Info) View(c *gin.Context) {
+func View(c *gin.Context) {
 	key, err := husk.ParseKey(c.Param("key"))
 
 	if err != nil {
-		return http.StatusBadRequest, err
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
 	}
 
 	record, err := core.GetEntity(key)
 
 	if err != nil {
-		return http.StatusNotFound, err
+		c.AbortWithError(http.StatusNotFound, err)
+		return
 	}
 
-	return http.StatusOK, record
+	c.JSON(http.StatusOK, record)
 }
 
 // @Title CreateEntity
@@ -56,19 +57,21 @@ func (req *Info) View(c *gin.Context) {
 // @Success 200 {map[string]string} map[string]string
 // @Failure 403 body is empty
 // @router / [post]
-func (req *Info) Create(c *gin.Context) {
+func Create(c *gin.Context) {
 	var entry core.Entity
 	err := ctx.Body(&entry)
 
 	if err != nil {
-		return http.StatusBadRequest, err
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
 	}
 
 	rec, err := entry.Create()
 
 	if err != nil {
-		return http.StatusNotFound, err
+		c.AbortWithError(http.StatusNotFound, err)
+		return
 	}
 
-	return http.StatusOK, rec
+	c.JSON(http.StatusOK, rec)
 }
